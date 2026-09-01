24000bf17fe10f617973679b2a3cc20d4a6bc672
Felipe Santos <felipeslaik@gmail.com>
2026-09-01 11:31:35 -0300

    Finalização do caixa interativo

--- caixa.py
+++ caixa.py
@@ -1,28 +1,50 @@
-
-continuar = "sim"
+opcao = ""
 produtos = []
-
-while continuar == "sim":
-    nomeProduto = input("Qual o nome do produto? ")
-    precoProduto = float(input("Qual o preço do produto? "))
-    quantidadeComprada = int(input("Quantas unidades será comprada? "))
-    produto = {
-    "nome": nomeProduto,
-    "preco": precoProduto,
-    "quantidade": quantidadeComprada
-
-}
-
-    produtos.append(produto)
-    continuar = input("Deseja adicionar outro produto? (sim/não) ")
+while opcao != "4":
+    print("===== CAIXA =====")
+    print("1 - Adicionar produto")
+    print("2 - Remover produto")
+    print("3 - Ver produtos")
+    print("4 - Finalizar compra")
     
-total = 0
+    opcao = input("Escolha uma opção: ")
 
-for produto in produtos:
-    print(produto["nome"])
-    print(produto["preco"])
-    print(produto["quantidade"])
-    subtotal = produto["preco"] * produto["quantidade"]
-    total = total + subtotal
+    if opcao == "1":
+        nomeProduto = input("Qual o nome do produto? ")
+        precoProduto = float(input("Qual o preço do produto? "))
+        quantidadeComprada = int(input("Quantas unidades será comprada? "))
+    
+        produto = {
+        "nome": nomeProduto,
+        "preco": precoProduto,
+        "quantidade": quantidadeComprada
+        }
+        produtos.append(produto)
+     
+    elif opcao == "2":
+        itenRemover = int(input("Qual item deseja remover? (Digite o número correspondente) "))
+        indiceRemover = itenRemover - 1
+        produtoRemover = produtos[indiceRemover]
+        del produtos[indiceRemover]
+        print("Produto removido:", produtoRemover["nome"])
 
-print(f"Total: R$ {total:.2f}")
+    elif opcao == "3":
+        for i, produto in enumerate(produtos, start=1):
+            print(i, "-", produto["nome"])
+            print(f"Preço R$ {produto["preco"]:.2f}")
+            print(f"Quantidade: {produto["quantidade"]}")
+
+    elif opcao == "4":
+         total = 0
+         for produto in produtos:
+            subtotal = produto["preco"] * produto["quantidade"]
+            total = total + subtotal
+
+         print(f"Total: R$ {total:.2f}")
+         print("Sair")
+
+
+
+
+
+
